62336d3bac03c7303900d3b76507102e520ae160
peterpham1102 <youngbuffalo1102@gmail.com>
2020-11-18 15:17:08 +0700

    Nov 18th Transport

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary';
+
+const sideDrawer = (props) => {
+    //...
+    return (
+        <Auxiliary>
+            <Backdrop show />
+            <div className={classes.SideDrawer}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Auxiliary>
+    );
+};
+
+export default sideDrawer;
